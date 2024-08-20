a919d0e945f2c295eeef03fe5e23504abfdcc4a6
Arish <arishchowdhury.2021@gmail.com>
2024-08-20 22:43:16 +0600

    I did the assignment

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+import java.io.*;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter date of Birth");
+        int a = s.nextInt();
+
+        System.out.print("Enter month of Birth");
+        int b = s.nextInt();
+
+        System.out.println("Enter year of Birth");
+        int c = s.nextInt();
+
+
+        }
+    }
